b0e74d37f349651adb663417a9fe5dd67ff2fcd6
coneve1414 <36418634+coneve1414@users.noreply.github.com>
2020-12-02 11:51:49 -0500

    .

--- public/firebase/registerPage.js
+++ public/firebase/registerPage.js
@@ -12,9 +12,7 @@ function initApp2() {
         function createUser() {
           console.log('starting user create');
             var email2 = document.getElementById('email').value;
-            console.log(email2);
             var password2 = document.getElementById('password').value;
-            console.log(password2);
             if (email2.length < 4) {
               alert('Please enter an email address.');
               return;
@@ -23,8 +21,6 @@ function initApp2() {
               alert('Please enter a longer password.');
               return;
             }
-            console.log(document.getElementById('verificationCode').value);
-            console.log("sending to orgVerificationCode");
             getOrgCode(document.getElementById('verificationCode').value); //sending data to orgVerificationCode.js
         }
         document.getElementById('createAccount').addEventListener('click', createUser, false);
