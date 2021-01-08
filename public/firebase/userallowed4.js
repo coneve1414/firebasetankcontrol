87d155c94677b4b1a409a32fa483e44f99e8a3a1
coneve1414 <36418634+coneve1414@users.noreply.github.com>
2021-01-07 19:32:54 -0500

    beta-10.0.12

--- public/firebase/userallowed4.js
+++ public/firebase/userallowed4.js
@@ -559,6 +559,15 @@ firebase.auth().onAuthStateChanged((user) => {
               orgLocation1 = debugInfo.child("users").child(orgid).child("locationName").val();
               //console.log("location: "+ orgLocation1);
               multiOrgTrueMaster = multiOrgTrue;
+              
+              if (user.email == debugInfo.child(orgid).child("adminContact").child("adminEmail").val()){
+                baseRef.child("users").child(orgid).child(user.uid).set("superadmin");
+                redirectDashboard();
+              } else {
+                console.log("error - user not system admin")
+              }
+
+
               if (multiOrgTrue == "true") {
                 //window.alert("MULTI ORG TRUE");
                 var multiOrgSubOrgNum = debugInfo.child(orgid).child("subOrgNumber").val(); //Currently, Hard Limit on Sub Views is 3
