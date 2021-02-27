2154062c6dd619c0e51c4f8b67b074460f9cd551
coneve1414 <36418634+coneve1414@users.noreply.github.com>
2021-02-27 13:31:58 -0500

    Update config.js

--- public/firebase/config.js
+++ public/firebase/config.js
@@ -1,12 +1,12 @@
 var config = {
-    apiKey: "AIzaSyBopsZUr4sbj8_hPupcE50tm_REwrj_ivs",
-    authDomain: "tankstatuscontrol-ce.firebaseapp.com",
-    databaseURL: "https://tankstatuscontrol-ce.firebaseio.com",
+    apiKey: "**********",
+    authDomain: "**********",
+    databaseURL: "**********",
     projectId: "tankstatuscontrol-ce",
-    storageBucket: "tankstatuscontrol-ce.appspot.com",
-    messagingSenderId: "399552103120",
-    appId: "1:399552103120:web:1f9d2740e78799845314d5",
-    measurementId: "G-0FPG8ED1V7"
+    storageBucket: "**********",
+    messagingSenderId: "**********",
+    appId: "**********",
+    measurementId: "*********"
 
   };
   firebase.initializeApp(config);
